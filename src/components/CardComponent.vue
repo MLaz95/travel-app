e32477a80d960341a825692779ddd380e88a8de1
MLaz95 <lazzari78.ml@gmail.com>
2024-08-28 12:59:19 +0200

    added font

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -26,8 +26,8 @@ export default{
 
     
     <div class="h-100 d-flex flex-column justify-content-center px-3 text-white">
-        <h2 class="">{{ stop.date.getDate() }}-{{ stop.date.getMonth() }}-{{ stop.date.getFullYear() }}</h2>
-        <h1 class="text-uppercase">{{ stop.name }}</h1>
+        <h2 class="title-font">{{ stop.date.getDate() }}-{{ stop.date.getMonth() }}-{{ stop.date.getFullYear() }}</h2>
+        <h1 class="text-uppercase title-font">{{ stop.name }}</h1>
         <p>{{ stop.description }}</p>
         <button @click="setActive(stop)" class="btn btn-outline-light" >I'm Here!</button>
         
