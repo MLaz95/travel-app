785e3fbce66339cf2ec0bdf4e721faf0417fd4cf
MLaz95 <lazzari78.ml@gmail.com>
2024-08-30 17:01:10 +0200

    slide component style and offcanvas

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ export default{
   data(){
     return{
       store,
+      offCanvasIndex: 0,
     }
   },
 
@@ -20,8 +21,16 @@ export default{
   },
 
   mounted(){
-  // console.log(store.trip)
-  }
+    const exampleOffCanvas = document.getElementById('offcanvasExample');
+    console.log(exampleOffCanvas)
+    if(exampleOffCanvas){
+      exampleOffCanvas.addEventListener('show.bs.offcanvas', event => {
+        // retrieve slide index from the button that opened the offcanvas
+        this.offCanvasIndex = event.relatedTarget.getAttribute('data-bs-index')
+      })
+    }
+  },
+
 }
 
 </script>
@@ -49,6 +58,29 @@ export default{
         
       </div>
   </div>
+
+  <!-- Offcanvas triggered from CardComponent-->
+  <div class="offcanvas offcanvas-start w-50" data-bs-backdrop="false" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">{{ store.trip.stops[offCanvasIndex].name }}</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+    </div>
+    <div class="dropdown mt-3">
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+        Dropdown button
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
 </template>
 
 <style scoped>
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -26,12 +26,49 @@ export default{
 
     
     <div class="h-100 d-flex flex-column justify-content-center px-3 text-white">
-        <h2 class="title-font">{{ stop.date.getDate() }}-{{ stop.date.getMonth() }}-{{ stop.date.getFullYear() }}</h2>
-        <h1 class="text-uppercase title-font">{{ stop.name }}</h1>
-        <p>{{ stop.description }}</p>
-        <button @click="setActive(stop)" class="btn btn-outline-light" >I'm Here!</button>
-        
+        <div class="d-flex justify-content-between align-items-center mb-1">
+            <h2 class="title-font m-1">{{ stop.date.getDate() }}-{{ stop.date.getMonth() }}-{{ stop.date.getFullYear() }}</h2>
+
+            <!-- select current stop button -->
+            <button @click="setActive(stop)" class="btn btn-outline-light pin_btn" :class="{active: store.trip.active == stop.id}">
+                <i class="fa-solid fa-map-location-dot"></i>
+            </button>
+        </div>
+
+        <!-- link triggers offcanvas -->
+        <a class="text-decoration-none text-white my_link" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" :data-bs-index="stop.id" aria-controls="offcanvasExample" href="">
+            <h1 class="text-uppercase title-font">{{ stop.name }}</h1>
+            <p>{{ stop.description }}</p>    
+        </a>
+
+
     </div>
+    
     
 
 </template>
+
+<style lang="scss" scoped>
+
+h1{
+    font-size: 100px;
+}
+
+h2{
+    font-size: 50px
+}
+
+.pin_btn{
+    width: 45px;
+    height: 45px;
+}
+
+.my_link{
+    transition: background-color 0.3s;
+
+    &:hover{
+        background-color: rgba($color: white, $alpha: 0.3);
+    }
+}
+
+</style>
