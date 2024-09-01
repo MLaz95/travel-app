93d8bc97eac919be2aecb12497fb6135082ad5bc
MLaz95 <lazzari78.ml@gmail.com>
2024-09-01 13:15:28 +0200

    optimized map changes

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -7,6 +7,7 @@ import tt from '@tomtom-international/web-sdk-maps'
         data(){
             return {
                 map: null,
+                markers: [],
                 store,
             }
         },
@@ -17,16 +18,19 @@ import tt from '@tomtom-international/web-sdk-maps'
         },
 
         watch:{
-            'store.trip.currentSlide'(newValue){
-                this.initializeMap()
+            // updates map center when scrolling through slides
+            'store.trip.currentSlide'(){
+                this.updateMapCenter()
             },
 
-            'store.trip.active'(newValue){
-                this.initializeMap()
+            // updates marker icon when new active stop is set
+            'store.trip.active'(newValue, oldValue){
+                this.updateMarkers(newValue, oldValue)
             }
         },
 
         methods: {
+            // creates the map
             initializeMap(){
                 const map = tt.map({
                     key: 'NE8Tzmv4CU0XVlD9o90M6K7ysBeHNLBp',
@@ -35,20 +39,39 @@ import tt from '@tomtom-international/web-sdk-maps'
                     zoom: 8,
                 })
 
+                // sets a marker on the map for each stop
                 store.trip.stops.forEach((stop, index) => {
-                    // sets appropriate marker on map
+                    // maps are saved in markers array so they can be modified later
                     if(store.trip.active == index){
                         let element = document.createElement('div')
                         element.id = "active_marker"
+                        this.markers.push(element)
                         new tt.Marker({element: element}).setLngLat(stop.coordinates).addTo(map)
                     }else{
                         let element = document.createElement('div')
-                        element.id = "marker"
+                        element.className += " marker"
+                        this.markers.push(element)
                         new tt.Marker({element: element}).setLngLat(stop.coordinates).addTo(map)
                     }
                 });
                 this.map = map;
-                console.log(this.map)
+            },
+
+            // changes center location of the map based on currentSlide value
+            updateMapCenter(){
+                this.map.flyTo({
+                    "center": this.store.trip.stops[this.store.trip.currentSlide].coordinates,
+                    "zoom": 8.5,
+                })
+            },
+
+            // reassigns the marker class and active_marker id to match current active stop value
+            updateMarkers(newValue, oldValue){
+                this.markers[oldValue].id = ""
+                this.markers[oldValue].className += " marker"
+
+                this.markers[newValue].classList.remove('marker')
+                this.markers[newValue].id = "active_marker"
             },
         },
     }
@@ -64,7 +87,7 @@ import tt from '@tomtom-international/web-sdk-maps'
     width: 100%;
 }
 
-#marker{
+.marker{
     background-image: url('/src/assets/img/map-pin-solid.svg');
     background-size: cover;
     width: 25px;
